5894aded6cff9ab10249be83367c9281fb684b0a
Brian Ketelsen <bketelsen@gmail.com>
2017-12-05 17:46:20 +0000

    documentation: cleanup cobra help, add environment and config information to README

--- cmd/root.go
+++ cmd/root.go
@@ -41,14 +41,12 @@ var taint string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "virtual-kubelet",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "virtual-kubelet provides a virtual kubelet interface for your kubernetes cluster.",
+	Long: `virtual-kubelet implements the Kubelet interface with a pluggable 
+backend implementation allowing users to create kubernetes nodes without running the kubelet.
+This allows users to schedule kubernetes workloads on nodes that aren't running Kubernetes.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(kubeConfig)
 		f, err := vkubelet.New(nodeName, operatingSystem, kubeNamespace, kubeConfig, taint, provider, providerConfig)
 		if err != nil {
 			log.Fatal(err)
