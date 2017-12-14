0c10accf41f7a8716e997696244d2f60d5734dc1
Robbie Zhang <junjiez@microsoft.com>
2017-12-14 20:32:04 +0000

    Configure KubeConfig from Script (#24)
    * Configure KubeConfig from Script

--- scripts/createCredentials.sh
+++ scripts/createCredentials.sh
@@ -14,3 +14,26 @@ cat <<EOF > ${outputPathCredsfile}
   "managementEndpointUrl": "$managementEndpointUrl"
 }
 EOF
+
+# This will build the kubeConfig during the CI
+cat <<EOF > ${outputPathKubeConfigFile}
+---
+apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: "$kubeConfigCertificateAuthorityData"
+    server: $kubeConfigServer
+  name: "aci-connector-k8s"
+contexts:
+- context:
+    cluster: "aci-connector-k8s"
+    user: "aci-connector-k8s-admin"
+  name: "aci-connector-k8s"
+current-context: "aci-connector-k8s"
+kind: Config
+users:
+- name: "aci-connector-k8s-admin"
+  user:
+    client-certificate-data: "$kubeConfigClientCertificateData"
+    client-key-data: "$kubeConfigClientKeyData"
+EOF
