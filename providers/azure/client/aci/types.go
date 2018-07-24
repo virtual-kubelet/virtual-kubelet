36eb3db8a9577d2fc2b02303b46ef42694f1d59b
Jeremy Rickard <jeremy.rickard@microsoft.com>
2018-07-24 15:19:20 -0700

    Adding network policy reference (#269)

--- providers/azure/client/aci/types.go
+++ providers/azure/client/aci/types.go
@@ -93,13 +93,23 @@ type ContainerGroupProperties struct {
 	OsType                   OperatingSystemTypes                 `json:"osType,omitempty"`
 	Volumes                  []Volume                             `json:"volumes,omitempty"`
 	InstanceView             ContainerGroupPropertiesInstanceView `json:"instanceView,omitempty"`
+<<<<<<< HEAD
 	Diagnostics              *ContainerGroupDiagnostics           `json:"diagnostics,omitempty"`
+=======
+	NetworkProfile           *NetworkProfileDefinition            `json:"networkProfile,omitempty"`
+>>>>>>> Adding network policy reference (#269)
 }
 
 // ContainerGroupPropertiesInstanceView is the instance view of the container group. Only valid in response.
 type ContainerGroupPropertiesInstanceView struct {
 	Events []Event `json:"events,omitempty"`
 	State  string  `json:"state,omitempty"`
+}
+
+// NetworkProfileDefinition is the network profile definition. ID should be of the form
+// /subscriptions/{subscriptionId} or /providers/{resourceProviderNamespace}/
+type NetworkProfileDefinition struct {
+	ID string `json:"id,omitempty"`
 }
 
 // ContainerGroupListResult is the container group list response that contains the container group properties.
