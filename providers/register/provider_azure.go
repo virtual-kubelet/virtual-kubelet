083f6dee05515d718bbb7103aea970966bb66afd
Brian Goff <cpuguy83@gmail.com>
2018-09-26 13:18:02 -0700

    Refactor provider init (#360)
    * Refactor provider init
    
    This moves provider init out of vkubelet setup, instead preferring to
    initialize vkubelet with a provider.
    
    * Split API server configuration from setup.
    
    This makes sure that configuration (which is done primarily through env
    vars) is separate from actually standing up the servers.
    
    This also makes sure to abort daemon initialization if the API servers
    are not able to start.

---
+++ providers/register/provider_azure.go
@@ -0,0 +1,23 @@
+// +build !no_azure_provider
+
+package register
+
+import (
+	"github.com/virtual-kubelet/virtual-kubelet/providers"
+	"github.com/virtual-kubelet/virtual-kubelet/providers/azure"
+)
+
+func init() {
+	register("azure", initAzure)
+}
+
+func initAzure(cfg InitConfig) (providers.Provider, error) {
+	return azure.NewACIProvider(
+		cfg.ConfigPath,
+		cfg.ResourceManager,
+		cfg.NodeName,
+		cfg.OperatingSystem,
+		cfg.InternalIP,
+		cfg.DaemonPort,
+	)
+}
