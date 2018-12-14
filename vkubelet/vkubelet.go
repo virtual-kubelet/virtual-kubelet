bc965528824de292d4045d9a2ebe4e4df7b84202
Brian Goff <cpuguy83@gmail.com>
2018-12-14 11:57:35 -0800

    Move http server handling up to daemon.
    This removes the burdern of dealing with listeners, http servers, etc in
    the core framework.
    
    Instead provide helpers to attach the appropriate routes to the
    caller's serve mux.
    
    With this change, the vkubelet package only helps callers setup HTTP
    rather than forcing a specific HTTP config on them.

--- vkubelet/vkubelet.go
+++ vkubelet/vkubelet.go
@@ -2,17 +2,13 @@ package vkubelet
 
 import (
 	"context"
-	"net"
-	"net/http"
 	"time"
 
-	pkgerrors "github.com/pkg/errors"
 	"go.opencensus.io/trace"
 	corev1 "k8s.io/api/core/v1"
 	corev1informers "k8s.io/client-go/informers/core/v1"
 	"k8s.io/client-go/kubernetes"
 
-	"github.com/virtual-kubelet/virtual-kubelet/log"
 	"github.com/virtual-kubelet/virtual-kubelet/manager"
 	"github.com/virtual-kubelet/virtual-kubelet/providers"
 )
@@ -45,29 +41,12 @@ type Config struct {
 	PodInformer     corev1informers.PodInformer
 }
 
-// ServeMux defines an interface used to attach routes to an existing http serve mux
-// It is used to enable callers creating a new server to completely manage their
-// own HTTP server while allowing us to attach the required routes to satisfy
-// the Kubelet interfaces.
-type ServeMux interface {
-	Handle(path string, h http.Handler, methods ...string) Route
-	HandleFunc(path string, h http.HandlerFunc, methods ...string) Route
-}
-
-// APIConfig is used to configure the API server of the virtual kubelet.
-// Routes required to satisfy the kubelet interface are attached to these.
-//
-// Callers should take care to namespace these serve muxes (muxi?) as they see
-// fit, however these routes get called by the Kubernetes API server when a user
-// requests things, e.g. `kubectl logs`.
-type APIConfig struct {
-	ServerMux  ServeMux
-	MetricsMux ServeMux
-}
-
 // New creates a new virtual-kubelet server.
 // This is the entrypoint to this package.
-func New(cfg Config) (*Server, error) {
+//
+// This creates but does not start the server.
+// You must call `Run` on the returned object to start the server.
+func New(cfg Config) *Server {
 	return &Server{
 		namespace:       cfg.Namespace,
 		nodeName:        cfg.NodeName,
@@ -81,47 +60,40 @@ func New(cfg Config) (*Server, error) {
 }
 
 // Run creates and starts an instance of the pod controller, blocking until it stops.
-func (s *Server) Run(ctx context.Context, cfg APIConfig) (retErr error) {
-	ctx = log.WithLogger(ctx, log.G(ctx))
-
-	attachPodRoutes(cfg.Provider, cfg.ServerMux)
-	go KubeletServerStart(cfg.Provider, apiL, cfg.CertPath, cfg.KeyPath)
-
-	if cfg.MetricsAddr != "" {
-		metricsL, err := net.Listen("tcp", cfg.MetricsAddr)
-		if err != nil {
-			return nil, pkgerrors.Wrap(err, "error setting up metrics listener")
-		}
-		defer func() {
-			if retErr != nil {
-				metricsL.Close()
-			}
-		}()
-		go MetricsServerStart(cfg.Provider, metricsL)
-	} else {
-		log.G(ctx).Info("Skipping metrics server startup since no address was provided")
-	}
-
+//
+// Note that this does not setup the HTTP routes that are used to expose pod
+// info to the Kubernetes API Server, such as logs, metrics, exec, etc.
+// See `AttachPodRoutes` and `AttachMetricsRoutes` to set these up.
+func (s *Server) Run(ctx context.Context) error {
 	if err := s.registerNode(ctx); err != nil {
 		return err
 	}
 
-	go func() {
-		tick := time.NewTicker(5 * time.Second)
-		defer tick.Stop()
-
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case <-tick.C:
-				ctx, span := trace.StartSpan(ctx, "syncActualState")
-				s.updateNode(ctx)
-				s.updatePodStatuses(ctx)
-				span.End()
-			}
-		}
-	}()
+	go s.providerSyncLoop(ctx)
 
 	return NewPodController(s).Run(ctx, s.podSyncWorkers)
+}
+
+// providerSyncLoop syncronizes pod states from the provider back to kubernetes
+func (s *Server) providerSyncLoop(ctx context.Context) {
+	// TODO(@cpuguy83): Ticker does not seem like the right thing to use here. A
+	// ticker keeps ticking while we are updating state, which can be a long
+	// operation. This would lead to just a constant re-sync rather than sleeping
+	// for 5 seconds between each loop.
+	//
+	// Leaving this note here as fixing is out of scope for my current changeset.
+	tick := time.NewTicker(5 * time.Second)
+	defer tick.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-tick.C:
+			ctx, span := trace.StartSpan(ctx, "syncActualState")
+			s.updateNode(ctx)
+			s.updatePodStatuses(ctx)
+			span.End()
+		}
+	}
 }
