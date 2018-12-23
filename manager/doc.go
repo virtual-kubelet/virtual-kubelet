8a5d80984bf2aa118f5ef2bac3902b5059d45378
Brian Goff <cpuguy83@gmail.com>
2018-12-23 09:26:59 -0800

    Adds some package docs
    Was just browing godoc and noticed we are missing some docs that would
    be quite useful.

---
+++ manager/doc.go
@@ -0,0 +1,7 @@
+// Package manager provides access to kubernetes resources for providers.
+//
+// DEPRECATION WARNING:
+// Though this package is still in use, it should be considered deprecated as it
+// is just wrapping a k8s client and not much else.
+// Implementers should look at replacing their use of this with something else.
+package manager
