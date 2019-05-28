acb36b63423325ca4a94b01810129ef516492193
pmould <paul.mould1@gmail.com>
2019-05-28 12:16:06 -0500

    #622 add support for new regions for aws fargate (#623)
    * #622 add support for new regions for aws fargate

--- providers/aws/fargate/region.go
+++ providers/aws/fargate/region.go
@@ -12,12 +12,17 @@ var (
 	// FargateRegions are AWS regions where Fargate is available.
 	FargateRegions = Regions{
 		"ap-northeast-1", // Asia Pacific (Tokyo)
+		"ap-northeast-2",  // Asia Pacific (Seoul)
 		"ap-southeast-1", // Asia Pacific (Singapore)
 		"ap-southeast-2", // Asia Pacific (Sydney)
+		"ap-south-1",     // Asia Pacific (Mumbai)
+		"ca-central-1",   // Canada (Central)
 		"eu-central-1",   // EU (Frankfurt)
 		"eu-west-1",      // EU (Ireland)
+		"eu-west-2",      // EU (London)
 		"us-east-1",      // US East (N. Virginia)
 		"us-east-2",      // US East (Ohio)
+		"us-west-1",      // US West (N. California)
 		"us-west-2",      // US West (Oregon)
 	}
 )
