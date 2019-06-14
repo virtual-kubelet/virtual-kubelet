c6fdae931a4ec3c9dcca04481174e1c5b592bc9d
Brian Goff <cpuguy83@gmail.com>
2019-06-14 12:19:39 -0700

    Remove version package (#667)
    This moves version declaration into cmd/virtual-kubelet/main.go
    From here values are passed down into the things that need it.

--- cmd/virtual-kubelet/commands/version/version.go
+++ cmd/virtual-kubelet/commands/version/version.go
@@ -18,17 +18,16 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
-	"github.com/virtual-kubelet/virtual-kubelet/version"
 )
 
 // NewCommand creates a new version subcommand command
-func NewCommand() *cobra.Command {
+func NewCommand(version, buildTime string) *cobra.Command {
 	return &cobra.Command{
 		Use:   "version",
 		Short: "Show the version of the program",
 		Long:  `Show the version of the program`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("Version: %s, Built: %s\n", version.Version, version.BuildTime)
+			fmt.Printf("Version: %s, Built: %s\n", version, buildTime)
 		},
 	}
 }
