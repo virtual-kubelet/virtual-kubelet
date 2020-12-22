028e976421b6e9a55415d78060882b2179cb2aab
guwe <guwe@microsoft.com>
2020-12-22 06:47:27 +0000

    License 2020

--- node/api/middleware.go
+++ node/api/middleware.go
@@ -1,4 +1,4 @@
-// Copyright © 2017 The virtual-kubelet authors
+// Copyright © 2020 The virtual-kubelet authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
