61bb87a195f4e7a9bdd262d0e2f825db09376b92
Arnav Arnav <fnuarnav@microsoft.com>
2023-03-23 12:08:46 -0700

    return mock metrics data for all pods and contianers

--- cmd/virtual-kubelet/internal/provider/mock/mock.go
+++ cmd/virtual-kubelet/internal/provider/mock/mock.go
@@ -509,43 +509,124 @@ func (p *MockProvider) GetStatsSummary(ctx context.Context) (*stats.Summary, err
 	return res, nil
 }
 
+
+func (p *MockProvider) generateMockMetrics(metricsMap map[string][]*dto.Metric, resourceType string, label []*dto.LabelPair) map[string][]*dto.Metric {
+	var (
+		cpuMetricSuffix            = "_cpu_usage_seconds_total"
+		memoryMetricSuffix         = "_memory_working_set_bytes"
+		dummyValue                 = float64(100)
+	)
+
+	if metricsMap == nil {
+		metricsMap = map[string][]*dto.Metric{}
+	}
+
+	finalCpuMetricName    := resourceType + cpuMetricSuffix
+	finalMemoryMetricName := resourceType + memoryMetricSuffix
+
+	newCPUMetric := dto.Metric {
+		Label: label,
+		Counter: &dto.Counter {
+			Value: &dummyValue,
+		},
+	}
+	newMemoryMetric := dto.Metric {
+		Label: label,
+		Gauge: &dto.Gauge {
+			Value: &dummyValue,
+		},
+	}
+	// if metric family exists add to metric array
+	if cpuMetrics, ok := metricsMap[finalCpuMetricName]; ok {
+		metricsMap[finalCpuMetricName] = append(cpuMetrics, &newCPUMetric)
+	} else {
+		metricsMap[finalCpuMetricName] = []*dto.Metric{&newCPUMetric}
+	}
+	if memoryMetrics, ok := metricsMap[finalMemoryMetricName]; ok {
+		metricsMap[finalMemoryMetricName] = append(memoryMetrics, &newMemoryMetric)
+	} else {
+		metricsMap[finalMemoryMetricName] = []*dto.Metric{&newMemoryMetric}
+	}
+
+	return metricsMap
+}
+
+func (p *MockProvider) getMetricType(metricName string) *dto.MetricType {
+	var (
+		dtoCounterMetricType       = dto.MetricType_COUNTER
+		dtoGaugeMetricType         = dto.MetricType_GAUGE
+		cpuMetricSuffix            = "_cpu_usage_seconds_total"
+		memoryMetricSuffix         = "_memory_working_set_bytes"
+	)
+	if strings.HasSuffix(metricName, cpuMetricSuffix) {
+		return &dtoCounterMetricType
+	}
+	if strings.HasSuffix(metricName, memoryMetricSuffix) {
+		return &dtoGaugeMetricType
+	}
+
+	return nil
+}
+
 func (p *MockProvider) GetMetricsResource(ctx context.Context) ([]*dto.MetricFamily, error) {
 	var span trace.Span
 	ctx, span = trace.StartSpan(ctx, "GetMetricsResource") //nolint: ineffassign,staticcheck
 	defer span.End()
 
 	var (
-		dtoCounterMetricType = dto.MetricType_COUNTER
-		dtoGaugeMetricType   = dto.MetricType_GAUGE
-		podCPUMetricName     = "pod_cpu_usage_seconds_total"
-		podMemoryMetricName  = "pod_memory_working_set_bytes"
-		dummyValue           = float64(100)
+		nodeNameStr = "NodeName"
+		podNameStr = "PodName"
+		containerNameStr = "containerName"
 	)
-
-	res := []*dto.MetricFamily{
+	nodeLabels := []*dto.LabelPair{
 		{
-			Name: &podCPUMetricName,
-			Type: &dtoCounterMetricType,
-			Metric: []*dto.Metric{
-				{
-					Counter: &dto.Counter{
-						Value: &dummyValue,
-					},
-				},
-			},
-		},
-		{
-			Name: &podMemoryMetricName,
-			Type: &dtoGaugeMetricType,
-			Metric: []*dto.Metric{
-				{
-					Gauge: &dto.Gauge{
-						Value: &dummyValue,
-					},
-				},
-			},
+			Name:  &nodeNameStr,
+			Value: &p.nodeName,
 		},
 	}
+
+	metricsMap := p.generateMockMetrics(nil, "node", nodeLabels)
+	for _, pod := range p.pods {
+		podLabels := []*dto.LabelPair {
+			{
+				Name: &nodeNameStr,
+				Value: &p.nodeName,
+			},
+			{
+				Name: &podNameStr,
+				Value: &pod.Name,
+			},
+		}
+		metricsMap = p.generateMockMetrics(metricsMap, "pod", podLabels)
+		for _, container:= range pod.Spec.Containers {
+			containerLabels := []*dto.LabelPair {
+				{
+					Name: &nodeNameStr,
+					Value: &p.nodeName,
+				},
+				{
+					Name: &podNameStr,
+					Value: &pod.Name,
+				},
+				{
+					Name: &containerNameStr,
+					Value: &container.Name,
+				},
+			}
+			metricsMap = p.generateMockMetrics(metricsMap, "container", containerLabels)
+		}
+	}
+
+	res := []*dto.MetricFamily{}
+	for metricName, metrics := range metricsMap {
+		metricFamily := dto.MetricFamily {
+			Name: &metricName,
+			Type: p.getMetricType(metricName),
+			Metric: metrics,
+		}
+		res = append(res, &metricFamily)
+	}
+
 	return res, nil
 }
 
